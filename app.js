9174308ccd288765a90bb0ff10c27ba6144979d8
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-07-15 04:12:44 +0000

    [7章]

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 $(function(){
-  $('.box1').slideDown(function (){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'width': '200px',
-      'height': '100px'
-    }).slideUp();
+  $('button').on('click', function(){
+    $('ul').children().css('color', 'red');
   });
 });
